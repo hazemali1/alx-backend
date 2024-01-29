918890c5a9ebff045802fde80f0d56a1de799219
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-29 20:22:34 +0200

    start and end indexes

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""csv, math, typing modules"""
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """tuples index"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """get page"""
+        assert isinstance(page, int) and page > 0 and isinstance(page_size, int) and page_size > 0
+        print([self.dataset()[index_range(page, page_size)[0], index_range(page, page_size)[1]]])
