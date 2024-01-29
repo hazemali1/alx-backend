4205be67287080360b92f62eec9ef0fbed72d7f3
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-29 22:00:52 +0200

    class

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -41,6 +41,7 @@ class Server:
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """get the hyper index"""
+        index = index + 1
         return {
             "index": index,
             "next_index": index + page_size,
