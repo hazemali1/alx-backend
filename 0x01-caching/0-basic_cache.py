794dcd02f4fac545f32ec5a6437858967673928f
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-31 18:35:01 +0200

    basic

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""  Basic dictionary """
+"""import class"""
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ Class that inherits from BaseCaching and is a caching system
-        This caching system doesn’t have limit """
+    """Basic"""
     def put(self, key, item):
-        """ Assign to the dictionary """
-        if key and item:
+        """put data"""
+        if key is not None or item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Return the value linked """
-        if key is None or self.cache_data.get(key) is None:
+        """get data"""
+        if key is None or key not in self.cache_data.keys():
             return None
-        return self.cache_data.get(key)
+        return self.cache_data[key]
