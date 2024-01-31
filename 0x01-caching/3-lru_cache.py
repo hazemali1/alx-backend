d923e79b20b62e8ba8c7a628f0a5fc9be8332d5b
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-31 20:03:50 +0200

    basic

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""import BaseCaching class"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRUCache caching implementation"""
+    def put(self, key, item):
+        """put data into the cache"""
+        if key and item and len(self.cache_data) < self.MAX_ITEMS:
+            self.cache_data[key] = item
+        elif key and item and key in self.cache_data.keys():
+            self.cache_data.pop(key)
+            self.cache_data[key] = item
+        elif key and item:
+            r = list(self.cache_data.keys())[0]
+            self.cache_data.pop(r)
+            print("DISCARD: {}".format(r))
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """get data from the cache"""
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
