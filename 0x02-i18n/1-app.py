d66e49a980263e06334a055b8cd9b8a30882356a
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-06 15:07:17 +0200

    1 app

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""flask"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+class Config():
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """index html"""
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    """main"""
+    app.run(debug=True)
