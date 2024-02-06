ffbf7e32580c348f8734f2176060263e67f3bfe0
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-06 22:04:24 +0200

    2 app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -7,7 +7,7 @@ from flask_babel import Babel
 app = Flask(__name__)
 
 
-class Config():
+class Config:
     """Config class"""
     LANGUAGES = ["en", "fr"]
     Babel_default_locale = "en"
@@ -21,12 +21,12 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """accept languages"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.request.accept_languages.best_match(app.config['LANGUAGES'])
 
-@app.route("/")
+@app.route('/')
 def index():
     """index html"""
-    return render_template("2-index.html")
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
