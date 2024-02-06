dbbdc3dac6533080837887fffda080c2d1b37804
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-06 18:57:14 +0200

    3 app

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""flask"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config():
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """accept languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route("/")
+def index():
+    """index html"""
+    return render_template("2-index.html")
+
+
+if __name__ == '__main__':
+    """main"""
+    app.run(debug=True)
