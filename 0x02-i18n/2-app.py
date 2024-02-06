a9a47514ac86af30261d772dde69bdd394ecba4e
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-06 22:05:32 +0200

    2 app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -4,9 +4,6 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
-app = Flask(__name__)
-
-
 class Config:
     """Config class"""
     LANGUAGES = ["en", "fr"]
@@ -14,6 +11,7 @@ class Config:
     Babel_default_timezone = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
