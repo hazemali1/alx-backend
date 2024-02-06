426d06592bda381dd0f130ecd26df1766362c18a
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-06 18:44:03 +0200

    2 app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -21,7 +21,7 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """accept languages"""
-    return "no"
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 @app.route("/")
 def index():
