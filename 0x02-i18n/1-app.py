2c3ad287e35430ef2ac4bdf9738e72c05e024384
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-06 15:07:56 +0200

    1 app

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 class Config():
     """Config class"""
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 app.config.from_object(Config)
 babel = Babel(app)
